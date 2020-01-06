599e257cddb757b1adde75ba9f88c9bf9605b629
devilliersdutoit <devillierst@lightstone.co.za>
2020-01-06 15:44:52 +0200

    ddt commit from PyCharm

--- Code/WeatherAPI.py
+++ Code/WeatherAPI.py
@@ -1,12 +1,14 @@
 ## https://developer.weatherunlocked.com/signup
 
 import requests
-type = 'current'
-callstring = 'http://api.weatherunlocked.com/api/'+type+'/51.50,-0.12?app_id=0ca7c5a3&app_key=10fb8b0062bbcaf0934cedeb7a3cd23c'
+from requests import Response
+
+weather_type: str = 'current'
+call_string = 'http://api.weatherunlocked.com/api/' + weather_type + '/51.50,-0.12?app_id=0ca7c5a3&app_key=10fb8b0062bbcaf0934cedeb7a3cd23c'
  
-response = requests.get(callstring, headers={"Accept":"text/xml"})
+response: Response = requests.get(call_string, headers={'Accept': 'text/xml'})
 
-
+response.
 print(response.status_code)
 
 print(response.content)
