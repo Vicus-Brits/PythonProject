1cd169fca9f023c515bb9e03bcf10b2501becc6f
Vicus-Brits <59557634+Vicus-Brits@users.noreply.github.com>
2020-01-07 04:38:08 +0200

    Read PythonProject in README

--- Code/WeatherAPI.py
+++ Code/WeatherAPI.py
@@ -1,9 +1,12 @@
 ## https://developer.weatherunlocked.com/signup
 
+
 import requests
 from requests import Response
 import xml.etree.ElementTree as ET
 import pandas as pd
+import sqlite3
+
 
 #create data structure 
 data = {'temperature_c': [],
@@ -17,7 +20,7 @@ print(df_marks)
 
 # Get API data
 weather_type: str = 'current'
-call_string = 'http://api.weatherunlocked.com/api/' + weather_type + '/51.50,-0.12?app_id=0ca7c5a3&app_key=10fb8b0062bbcaf0934cedeb7a3cd23c'
+call_string = 'http://api.weatherunlocked.com/api/' + weather_type + '/-26.2041,28.0473?app_id=0ca7c5a3&app_key=10fb8b0062bbcaf0934cedeb7a3cd23c'
  
 
 Response = requests.get(call_string, headers={'Accept': 'text/xml'})
@@ -58,6 +61,32 @@ print('\n\nNew row added to DataFrame\n--------------------------')
 print(df_marks)
 
 
+# persist to SQL lite 
+conn = sqlite3.connect('test.db') # If the database does not exist, then it will be created and finally a database object will be returned.
+print ("Opened database successfully")
+
+#create table if not exists TableName (col1 typ1, ..., colN typN)
+
+conn.execute('''CREATE TABLE IF NOT EXISTS HEAT
+         (
+         temperature_c           REAL    NOT NULL,
+         temperature_f            REAL     NOT NULL);''')
+print ("Table created successfully")
+
+conn.execute("INSERT INTO HEAT (temperature_c,temperature_f) \
+       VALUES (1.1, 100.1)");
+print ("Test data inserted successfully")
 
 
+conn.execute("INSERT INTO HEAT (temperature_c,temperature_f) VALUES (?, ?)",
+          (temperature_c, temperature_f))
+print ("Python data inserted successfully")
 
+cursor = conn.execute("SELECT rowid, temperature_c, temperature_f FROM HEAT")
+for row in cursor:
+   print ("rowid = ", row[0])
+   print ("temperature_c = ", row[1])
+   print ("temperature_f = ", row[2])
+print ("Data read successfully")
+
+conn.close()
